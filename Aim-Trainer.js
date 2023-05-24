28636bf9b1de58ab64ba3e04377738805a598994
dwijy-piggy <45340809+dwijy-piggy@users.noreply.github.com>
2023-05-23 17:23:12 -0700

    Create Aim-Trainer.js

---
+++ Aim-Trainer.js
@@ -0,0 +1,2 @@
+javascript:document.getElementsByClassName("css-z6vxiy e6yfngs3")[0].dispatchEvent(new Event("mousedown", {bubbles: true}));
+for(var i = 0; i < 30; i++) document.getElementsByClassName("css-z6vxiy e6yfngs3")[0].dispatchEvent(new Event("mousedown", {bubbles: true}));
