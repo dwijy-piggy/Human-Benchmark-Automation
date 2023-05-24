f83d0957acb589bd485e368408741f98c3f99936
dwijy-piggy <45340809+dwijy-piggy@users.noreply.github.com>
2023-05-24 07:30:25 -0700

    Update Reaction-Time.js

--- Reaction-Time.js
+++ Reaction-Time.js
@@ -1,4 +1,5 @@
-javascript:new MutationObserver((ml) => {
+javascript:document.getElementsByClassName("css-42wpoy e19owgy79")[0].dispatchEvent(new Event('mousedown', {bubbles:true}));
+new MutationObserver((ml) => {
     for (const m of ml) {
         if (m.type === "attributes" && m.attributeName == "class" && m.target.className == "view-go e18o0sx0 css-saet2v e19owgy77") m.target.dispatchEvent(new Event('mousedown', {bubbles:true}));
     }
